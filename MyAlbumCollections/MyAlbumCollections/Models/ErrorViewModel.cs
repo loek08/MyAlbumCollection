97a8d0547b71e0bc287043a3b21bd30c4e342a3e
loek08 <558777@student.fontys.nl>
2026-03-24 14:31:44 +0100

    ssf

---
+++ MyAlbumCollections/MyAlbumCollections/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAlbumCollections.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
